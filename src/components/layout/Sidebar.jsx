33da6bbc8378cc224341145814a9e64050c7a6ff
Tofer-Cich <[tofercich@hotmail.com]>
2025-07-07 16:04:30 -0500

    Transitioned ProjectCards for smoother UX
    User experience is import for project section

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -5,10 +5,31 @@ function Sidebar({ gitHub, linkedIn}) {
                 <h1>Tofer Cich</h1>
                 <h2>Full-Stack Engineer</h2>
                 <p>I build scalable, full-stack applications&#8212;designed with precision and intent.</p>
-                <ul>
-                    <li>About</li>
-                    <li>Projects</li>
-                    <li>Technologies</li>
+                <ul className="sections">
+                    <li>
+                        <span className="svg-emdash">
+                            <svg className="emdash-line" viewBox="0 0 100 2" preserveAspectRatio="none">
+                                <line x1="0" y1='1' x2='0' y2='100' stroke='currentColor' strokeWidth='2' />
+                            </svg>
+                            About
+                        </span>
+                    </li>
+                    <li>
+                        <span className="svg-emdash">
+                            <svg className="emdash-line" viewBox="0 0 100 2" preserveAspectRatio="none">
+                                <line x1="0" y1='1' x2='0' y2='100' stroke='currentColor' strokeWidth='2' />
+                            </svg>
+                            Projects
+                        </span>
+                    </li>
+                    <li>
+                        <span className="svg-emdash">
+                            <svg className="emdash-line" viewBox="0 0 100 2" preserveAspectRatio="none">
+                                <line x1="0" y1='1' x2='0' y2='100' stroke='currentColor' strokeWidth='2' />
+                            </svg>
+                            Technologies
+                        </span>
+                    </li>
                 </ul>
             </div>
             <div id='sidebar-contacts'>
