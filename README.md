6d449cbb0f8f7b32561ed6aa9816d67aea943f73
Tofer-Cich <[tofercich@hotmail.com]>
2025-07-07 00:40:15 -0500

    Styled links in sidebar
    Sidebar needs icons for the socials

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Portfolio
 
 Welcome to my portfolio! This is where I'll keep my professional history and all previous work posted.
+
+## Who to attribute for icons
+- Pixel Perfect FlatIcon
+- Freepik FlatIcon
+- pocike FlatIcon
--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css';
 import About from './components/sections/About';
 import Projects from './components/sections/Projects';
 import react from './assets/react.svg';
+import gitHub from './assets/github.png';
+import linkedIn from './assets/linkedin.png';
 
 function App() {
 
@@ -32,14 +34,26 @@ function App() {
   return (
     <div id='screen'>
       <div id="sidebar">
-        <h1>Tofer Cich</h1>
-        <h2>Full-Stack Engineer</h2>
-        <p>I build scalable, full-stack applications&#8212;designed with precision and intent.</p>
-        <ul>
-          <li>About</li>
-          <li>Projects</li>
-          <li>Technologies</li>
-        </ul>
+        <div id='sidebar-title'>
+          <h1>Tofer Cich</h1>
+          <h2>Full-Stack Engineer</h2>
+          <p>I build scalable, full-stack applications&#8212;designed with precision and intent.</p>
+          <ul>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Technologies</li>
+          </ul>
+        </div>
+        <div id='sidebar-contacts'>
+          <ul>
+            <li>
+              <img src={gitHub} alt='Github'></img>
+            </li>
+            <li>
+              <img src={linkedIn} alt='LinkedIn'></img>
+            </li>
+          </ul>
+        </div>
       </div>
       <main>
         <About />
